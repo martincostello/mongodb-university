a37ee6dfbb745ff938e29c5eea43be65fddc9b73
martincostello <martin@martincostello.com>
2015-11-12 22:15:10 +0000

    Add (approximate) solution to 5.2.

---
+++ M101N/5.2/5.2.ps1
@@ -0,0 +1,44 @@
+# Remove the existing table, if any
+$cmd = 'use zipCodes
+db.smallZips.drop()'
+
+$cmd | mongo
+
+# Import the data
+Get-Content -Path 'small_zips.json' | mongoimport -d zipCodes -c smallZips
+
+$cmd = '
+db.smallZips.aggregate([
+	{
+		$match: {
+			"$or": [
+				{"state": "CA"},
+				{"state": "NY"}
+			],
+			"pop": { "$gt": 25000 }
+		}
+	},
+	{
+		$project: {
+			"_id": 0,
+			"state": "$state",
+			"city": "$city",
+			"zip": "$_id",
+			"pop": "$pop"
+		}
+	},
+	{
+		$group: {
+			"_id": { "state": "$state", "city": "$city" },
+			"pop": { $sum: "$pop" }
+		}
+	},
+	{
+		$group: {
+			"_id": null,
+			"average_population": { "$avg": "$pop" }
+		}
+	}
+]);'
+
+$cmd | mongo zipCodes
