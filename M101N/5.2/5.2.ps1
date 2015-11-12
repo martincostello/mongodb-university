6680d70c4befc8a57cbfb9f373af3d3f4b73ac3d
martincostello <martin@martincostello.com>
2015-11-12 22:39:07 +0000

    Add comments and fix tabs.

--- M101N/5.2/5.2.ps1
+++ M101N/5.2/5.2.ps1
@@ -7,38 +7,39 @@ $cmd | mongo
 # Import the data
 Get-Content -Path 'small_zips.json' | mongoimport -d zipCodes -c smallZips
 
+# Calculate the average population of cities in California and New York (taken together) with populations over 25,000.
 $cmd = '
 db.smallZips.aggregate([
-	{
-		$match: {
-			"$or": [
-				{"state": "CA"},
-				{"state": "NY"}
-			],
-			"pop": { "$gt": 25000 }
-		}
-	},
-	{
-		$project: {
-			"_id": 0,
-			"state": "$state",
-			"city": "$city",
-			"zip": "$_id",
-			"pop": "$pop"
-		}
-	},
-	{
-		$group: {
-			"_id": { "state": "$state", "city": "$city" },
-			"pop": { $sum: "$pop" }
-		}
-	},
-	{
-		$group: {
-			"_id": null,
-			"average_population": { "$avg": "$pop" }
-		}
-	}
+    {
+        $match: {
+            "$or": [
+                {"state": "CA"},
+                {"state": "NY"}
+            ],
+            "pop": { "$gt": 25000 }
+        }
+    },
+    {
+        $project: {
+            "_id": 0,
+            "state": "$state",
+            "city": "$city",
+            "zip": "$_id",
+            "pop": "$pop"
+        }
+    },
+    {
+        $group: {
+            "_id": { "state": "$state", "city": "$city" },
+            "pop": { $sum: "$pop" }
+        }
+    },
+    {
+        $group: {
+            "_id": null,
+            "average_population": { "$avg": "$pop" }
+        }
+    }
 ]);'
 
 $cmd | mongo zipCodes
--- M101N/5.3/5.3.ps1
+++ M101N/5.3/5.3.ps1
@@ -7,44 +7,45 @@ $cmd | mongo
 # Import the data
 Get-Content -Path 'grades.json' | mongoimport -d grades -c grades
 
+# Calculate the class with the best average student performance.
 $cmd = '
 db.grades.aggregate([
-	{
-		$unwind: "$scores"
-	},
-	{
-		$match: {
-			$or: [
-				{ "scores.type": "exam" },
-				{ "scores.type": "homework" }
-			]
-		}
-	},
-	{
-		$group: {
-			"_id": { "student_id": "$student_id", "class_id": "$class_id" },
-			"gpa": { $avg: "$scores.score" }
-		}
-	},
-	{
-		$group: {
-			"_id": "$_id.class_id",
-			"average": { $avg: "$gpa" }
-		}
-	},
-	{
-		$sort: { "average": -1 }
-	},
-	{
-		$project: {
-			"_id": 0,
-			"class_id": "$_id",
-			"average_student_gpa": "$average"
-		}
-	},
-	{
-		$limit: 1
-	}
+    {
+        $unwind: "$scores"
+    },
+    {
+        $match: {
+            $or: [
+                { "scores.type": "exam" },
+                { "scores.type": "homework" }
+            ]
+        }
+    },
+    {
+        $group: {
+            "_id": { "student_id": "$student_id", "class_id": "$class_id" },
+            "gpa": { $avg: "$scores.score" }
+        }
+    },
+    {
+        $group: {
+            "_id": "$_id.class_id",
+            "average": { $avg: "$gpa" }
+        }
+    },
+    {
+        $sort: { "average": -1 }
+    },
+    {
+        $project: {
+            "_id": 0,
+            "class_id": "$_id",
+            "average_student_gpa": "$average"
+        }
+    },
+    {
+        $limit: 1
+    }
 ]);'
 
 $cmd | mongo grades
