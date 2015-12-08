6c8cfc14d3f6f0d6e977402b9ae8ee2bbc0a1517
martincostello <martin@martincostello.com>
2015-12-08 10:15:41 +0000

    Application for exam question 7
    Console application for exam question 7 that deletes all of the images
    that are not in an album.

---
+++ M101N/Final/7/Question7/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace MartinCostelo.M101N.Exam.Question7
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+
+            Console.WriteLine("Press enter...");
+            Console.ReadLine();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            var client = new MongoClient();
+            var db = client.GetDatabase("photos");
+
+            var albums = db.GetCollection<BsonDocument>("albums");
+
+            var result = await albums
+                .Aggregate()
+                .Unwind("images")
+                .Group(@"{ _id: ""$images"" }")
+                .ToListAsync();
+
+            var nonOrphansIds = result
+                .Select((p) => (int)p["_id"])
+                .ToList();
+
+            var images = db.GetCollection<BsonDocument>("images");
+
+            var allImages = await images
+                .Find(new BsonDocument())
+                .ToListAsync();
+
+            List<int> orphanIds = new List<int>();
+
+            foreach (var image in allImages)
+            {
+                int id = (int)image["_id"];
+
+                if (!nonOrphansIds.Contains(id))
+                {
+                    await images.DeleteOneAsync(string.Format(CultureInfo.InvariantCulture, "{{_id: {0}}}", id));
+                }
+            }
+        }
+    }
+}
---
+++ M101N/Final/7/Question7/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("M101N.Exam.Question7")]
+[assembly: AssemblyDescription("Application that helps provide the answer to question 7 of the M101N final exam.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("M101N.Exam.Question7")]
+[assembly: AssemblyCopyright("Copyright © Martin Costello 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a620d1c7-a0b1-41ec-9a38-a915ac03dd34")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
